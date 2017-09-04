b15b758d50df86507ee29f87b80ca5bbf1f9431b
Nguyen Hieu Hiep <peihsen.doyle@gmail.com>
2017-09-05 00:10:13 +0700

    - support whisper simultaneously

--- Source/Showing.swift
+++ Source/Showing.swift
@@ -1,5 +1,9 @@
 import UIKit
 
+public func showWhisperSimultaneously(whisper message: Message, to: UINavigationController, action: WhisperAction = .show) {
+    WhisperFactory.init().craft(message, navigationController: to, action: action)
+}
+
 public func show(whisper message: Message, to: UINavigationController, action: WhisperAction = .show) {
   whisperFactory.craft(message, navigationController: to, action: action)
 }
@@ -10,6 +14,10 @@ public func show(shout announcement: Announcement, to: UIViewController, complet
 
 public func show(whistle murmur: Murmur, action: WhistleAction = .show(1.5)) {
   whistleFactory.whistler(murmur, action: action)
+}
+
+public func hideWhisperSimultaneously(whisperFrom from: UINavigationController, after: TimeInterval = 0) {
+    WhisperFactory.init().silentWhisper(from, after: after)
 }
 
 public func hide(whisperFrom from: UINavigationController, after: TimeInterval = 0) {
